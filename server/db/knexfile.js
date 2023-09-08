dabb90e2be7bc153cc112a3e95c24e4ee9f23b33
Gabriel Rawcliffe <gabriel.rawcliffe@gmail.com>
2023-09-08 20:16:11 +1200

    Knexfile production edit

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -35,12 +35,20 @@ export default {
 
   production: {
     client: 'sqlite3',
-    useNullAsDefault: true,
     connection: {
-      filename: '/app/storage/prod.sqlite3',
+      filename: '/app/storage/dev.sqlite3',
     },
-    pool: {
-      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
-    },
+    useNullAsDefault: true,
   },
 }
+
+//   production: {
+//     client: 'sqlite3',
+//     useNullAsDefault: true,
+//     connection: {
+//       filename: '/app/storage/prod.sqlite3',
+//     },
+//     pool: {
+//       afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
+//     },
+//   },
